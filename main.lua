adfd817961e7dd5025a05ba63de37245704c8493
Lerg <sergey.lerg@gmail.com>
2022-02-11 03:35:04 +0200

    Added fade out and looping.

--- main.lua
+++ main.lua
@@ -13,33 +13,39 @@ display.setDefault('background', 1)
 
 local xl, xr, y = display.contentWidth * .35, display.contentWidth * .65, display.contentCenterY
 local w, h = display.contentWidth * 0.3, 40
+local top_y = y - 200
+local y_step = 50
 
 local japanId, rainId, vacuumId
 
 widget.newButton{
-	x = xl, y = y - 200,
+	x = xl, y = top_y,
 	width = w, height = h,
 	label = 'Add Japan',
 	onRelease = function()
 		japanId = nativeaudio.addSource{
-			filename = 'sounds/music/japan_1.mp3'
+			filename = 'sounds/music/japan_1.mp3',
+			onComplete = function()
+				print('Japan ended.')
+			end
 		}
 		print('japan_1')
 	end}
 
 widget.newButton{
-	x = xr, y = y - 200,
+	x = xr, y = top_y,
 	width = w, height = h,
 	label = 'Add rain',
 	onRelease = function()
 		rainId = nativeaudio.addSource{
-			filename = 'sounds/sfx/rain.mp3'
+			filename = 'sounds/sfx/rain.mp3',
+			loop = true
 		}
 		print('rain')
 	end}
 
 widget.newButton{
-	x = xl, y = y - 120,
+	x = xl, y = top_y + y_step,
 	width = w, height = h,
 	label = 'Add vacuum',
 	onRelease = function()
@@ -50,7 +56,7 @@ widget.newButton{
 	end}
 
 widget.newButton{
-	x = xl, y = y - 40,
+	x = xr, y = top_y + y_step,
 	width = w, height = h,
 	label = 'Start',
 	onRelease = function()
@@ -59,7 +65,7 @@ widget.newButton{
 	end}
 
 widget.newButton{
-	x = xr, y = y - 40,
+	x = xl, y = top_y + 2 * y_step,
 	width = w, height = h,
 	label = 'Stop',
 	onRelease = function()
@@ -68,7 +74,7 @@ widget.newButton{
 	end}
 
 widget.newButton{
-	x = xl, y = y + 40,
+	x = xr, y = top_y + 2 * y_step,
 	width = w, height = h,
 	label = 'Play Japan',
 	onRelease = function()
@@ -77,7 +83,7 @@ widget.newButton{
 	end}
 
 widget.newButton{
-	x = xr, y = y + 40,
+	x = xl, y = top_y + 3 * y_step,
 	width = w, height = h,
 	label = 'Play rain',
 	onRelease = function()
@@ -86,7 +92,7 @@ widget.newButton{
 	end}
 
 widget.newButton{
-	x = xl, y = y + 120,
+	x = xr, y = top_y + 3 * y_step,
 	width = w, height = h,
 	label = 'Play vacuum',
 	onRelease = function()
@@ -95,7 +101,7 @@ widget.newButton{
 	end}
 
 widget.newButton{
-	x = xr, y = y + 120,
+	x = xl, y = top_y + 4 * y_step,
 	width = w, height = h,
 	label = 'Pause Japan',
 	onRelease = function()
@@ -104,7 +110,7 @@ widget.newButton{
 	end}
 
 widget.newButton{
-	x = xl, y = y + 200,
+	x = xr, y = top_y + 4 * y_step,
 	width = w, height = h,
 	label = 'Volume 50% Japan',
 	onRelease = function()
@@ -113,7 +119,7 @@ widget.newButton{
 	end}
 
 widget.newButton{
-	x = xr, y = y + 200,
+	x = xl, y = top_y + 5 * y_step,
 	width = w, height = h,
 	label = 'Set Info',
 	onRelease = function()
@@ -125,4 +131,25 @@ widget.newButton{
 			}
 		}
 		print('set info')
+	end}
+
+widget.newButton{
+	x = xr, y = top_y + 5 * y_step,
+	width = w, height = h,
+	label = 'Set Fade Out',
+	onRelease = function()
+		nativeaudio.setFadeOut{
+			delay = 5000,
+			duration = 10000
+		}
+		print('set fade out')
+	end}
+
+widget.newButton{
+	x = xl, y = top_y + 6 * y_step,
+	width = w, height = h,
+	label = 'Cancel Fade Out',
+	onRelease = function()
+		nativeaudio.cancelFadeOut()
+		print('cancel fade out')
 	end}
